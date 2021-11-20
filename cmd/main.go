a1702f837a287ff0d5626d005a0535d094469e57
lucasmmo <lucasmocerinomonteiro@gmail.com>
2021-11-20 02:25:21 -0300

    fix: docker image to push to heroku

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,9 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(rw, "Hello World")
